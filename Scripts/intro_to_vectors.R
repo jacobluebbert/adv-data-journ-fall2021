9529bdadab7b31c9ae67b93ebb084bbc259a520d
Jacob Luebbert <jdlfr5@umsystem.edu>
2021-09-01 09:10:25 -0500

    Added introduction script

---
+++ Scripts/intro_to_vectors.R
@@ -0,0 +1,22 @@
+# intro to vectors!
+
+name <- "jacob"
+id <- 1234
+class(name)
+class(id)
+names <- c("jacob", "luebbert")
+print(names)
+print(id)
+names[2]
+
+#intro to lists!
+
+lista <- list("hello", "goodbye", 1, 5)
+listb <- list(c("hello", 5), c("apple", 21, "blueberry"))
+length(listb)
+listb[1]
+
+#intro to data frames!
+
+df <- data.frame(student_id = c(1:5), name = c("David", "Bailey", "Grace", "Andrea", "Roshae"))
+df$name
