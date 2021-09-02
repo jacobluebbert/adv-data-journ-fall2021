20ab919dfad34d4cce6f941c9d8df7ffc531aff6
Jacob Luebbert <31893507+jacobluebbert@users.noreply.github.com>
2021-09-01 22:59:13 -0500

    Updated assignment 1

--- Assignments/assignment_1.R
+++ Assignments/assignment_1.R
@@ -1,7 +1,7 @@
 dispatch <- read.csv("Data/dispatch.csv")
 
 #1. What is "PolArea"?
-#2.How would I go about figuring out how many time a specific crime was committed in the past week?
+#2. How would I go about figuring out how many time a specific crime was committed in the past week?
 #3. How could I convert this data into a graph?
 #4. What hour of the day do the most crimes occur?
 #5. Where do the most crimes occur?
